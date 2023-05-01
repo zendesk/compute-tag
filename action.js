b2c9338c7bccbc22cbaf7dd2851114d8263a050d
Alex Plescan <alexpls@fastmail.com>
2023-05-01 11:05:29 +1000

    Increases page size used when listing commits
    I've run into frequent rate limits on a repository with over 180k commits.
    
    By default, `@octokit/plugin-paginate-rest` paginates at 30 items per page.
    
    This commit increases that to the maximum supported value of 100 items per page to lower chances of running into rate limits.
    
    From the octokit docs:
    
    > The per_page parameter is usually defaulting to 30, and can be set to up to 100, which helps retrieving a big amount of data without hitting the rate limits too soon.
    
    https://www.npmjs.com/package/@octokit/plugin-paginate-rest

--- action.js
+++ action.js
@@ -91,6 +91,10 @@ async function existingTags() {
 async function latestTagForBranch(allTags, branch) {
   const options = gitClient.rest.repos.listCommits.endpoint.merge({
     ...requestOpts,
+    // Set pagination per_page param to max allowed (100).
+    // Default is 30 per page, which hits rate limits on repositories with
+    // a lot of commits.
+    per_page: 100,
     sha: branch,
   })
 
